7a6452fa58abdd8a14075d3b60b8741b69b069a9
Charles Martell M. Dimalanta <charles.dimalanta10082003@gmail.com>
2026-03-17 21:40:12 +0800

    git push origin main

--- script.js
+++ script.js
@@ -94,6 +94,7 @@ let selectedCategories = []; // Array of selected category names
         "Chapter 39.1 (Workplace Tools)",
         "Chapter 39.2 (Cutting and Welding)",
         "Chapter 40.1 (Mixing and Measuring)",
+        "Chapter 40.2 (Product Defects)",
     ];
 ``
 // GET REFERENCES TO HTML ELEMENTS
